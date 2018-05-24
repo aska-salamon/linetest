d3218bff6e0d24dc6164fb71312dedf655d24e56
aska-salamon <39561166+aska-salamon@users.noreply.github.com>
2018-05-24 10:11:30 +0800

    Delete api.py

--- api.py
+++
@@ -1,7 +0,0 @@
-from linebot import (LineBotApi, WebhookHandler)
-
-# https://developers.line.me/console/
-# Channel Access Token
-line_bot_api = LineBotApi('UUKhBwd8KKReuezSkaLwYfIgrJVzq8cbmPTHz+M0FCepBpcrSUaktFHyb42hMjZiTZHI8AwZsA+E5itMRbCQo4UUbr2zehos+NePfVRiGIFQEsMfBpBjocuYxFs5ESZ5f8775WMnz92QgfralWo3jwdB04t89/1O/w1cDnyilFU=')
-# Channel Secret
-handler = WebhookHandler('81ddf38b14db2ad2e52f8ffb9f4bb012')
