caede51590716c753c1dd1d6706222ae3d4f06a2
aska-salamon <39561166+aska-salamon@users.noreply.github.com>
2018-05-24 09:04:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ from linebot.models import (
 app = Flask(__name__)
 
 # Channel Access Token
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+line_bot_api = LineBotApi('PW')
 # Channel Secret
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+handler = WebhookHandler('ASKA')
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
