c81090f9a5eaf39d4d41008424154d49d84ed581
aska-salamon <39561166+aska-salamon@users.noreply.github.com>
2018-05-24 10:36:12 +0800

    Update api.py

--- api.py
+++ api.py
@@ -2,6 +2,6 @@ from linebot import (LineBotApi, WebhookHandler)
 
 # https://developers.line.me/console/
 # Channel Access Token
-line_bot_api = LineBotApi('nx/QJURYoJp5itwu/0T4gf+ZIwuRIrT4UczQpu7j0KnZTQ8Ufjky5Th2fUGiv+4Al75xeWc+g9UvswshAOS3oj7WK9HliKZe3U1hKpIlP+hjtu+Qf8WCoH0UFD2HzjVfeKMMlRefiRM37MEQVXEN+wdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('UUKhBwd8KKReuezSkaLwYfIgrJVzq8cbmPTHz+M0FCepBpcrSUaktFHyb42hMjZiTZHI8AwZsA+E5itMRbCQo4UUbr2zehos+NePfVRiGIFQEsMfBpBjocuYxFs5ESZ5f8775WMnz92QgfralWo3jwdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
-handler = WebhookHandler('adf2f8c258655ed50846780126dd4a71')
+handler = WebhookHandler('81ddf38b14db2ad2e52f8ffb9f4bb012')
